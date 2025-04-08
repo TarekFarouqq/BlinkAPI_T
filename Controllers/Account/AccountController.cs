7999aa9dcf9319f944be28e901abab36fcc46938
Asmaa-A7med <asmaaahmed02758@gmail.com>
2025-04-08 15:07:27 +0200

    edit authntication

--- Controllers/Account/AccountController.cs
+++ Controllers/Account/AccountController.cs
@@ -7,6 +7,7 @@ using Blink_API.DTOs.IdentityDTOs;
 using Blink_API.Errors;
 using Blink_API.Services.AuthServices;
 using static System.Net.WebRequestMethods;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Blink_API.Controllers.Account
 {
@@ -16,16 +17,18 @@ namespace Blink_API.Controllers.Account
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IAuthServices _authServices;
+        private readonly IMemoryCache _cache;
         // add signIn manager for login : 
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly IEmailService _emailService;
 
-        public AccountController(UserManager<ApplicationUser> userManager, IAuthServices authServices, SignInManager<ApplicationUser> signInManager, IEmailService emailService)
+        public AccountController(UserManager<ApplicationUser> userManager, IAuthServices authServices, SignInManager<ApplicationUser> signInManager, IEmailService emailService, IMemoryCache cache)
         {
             _userManager = userManager;
             _authServices = authServices;
             _signInManager = signInManager;
             _emailService = emailService;
+            _cache = cache;
         }
 
         #region Register 
@@ -97,29 +100,7 @@ namespace Blink_API.Controllers.Account
         {
             if (ModelState.IsValid)
             {
-                // find user :
-                //var user = await _userManager.FindByEmailAsync(model.Email);
-                //if (user == null)
-                //{
-                //    return Unauthorized(new ApiResponse(401, "Invalid email or password"));
-                //}
-
-                //var result = await _signInManager.PasswordSignInAsync(user, model.Password, false, false);
-
-                //// check for login :
-                //if (result.Succeeded)
-                //{
-                //    // valid user , generate el token :
-                //    var token = await _authServices.CreateTokenAsync(user, _userManager);
-                //    return Ok(new { Token = token });
-                //}
-                //else
-                //{
-                //    // invalid login :
-                //    return Unauthorized(new ApiResponse(401, "Invalid login, Email or passward is not correct !"));
-                //}
-
-
+ 
                 var user = await _userManager.FindByNameAsync(model.Email);
                 if (user == null)
                 {
@@ -162,23 +143,36 @@ namespace Blink_API.Controllers.Account
         public async Task<IActionResult> ForgotPassword([FromBody] ForgetPassward model)
         {
             var user = await _userManager.FindByEmailAsync(model.Email);
-            if (user == null)
+            if (user == null || user.Email == null)
             {
                 return BadRequest(new { message = "Email not found" });
             }
-            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+            //var token = await _userManager.GeneratePasswordResetTokenAsync(user);
 
-            // Create the reset password link :
-            var resetLink = Url.Action("ResetPassword", "Account", new { email = user.Email, token }, Request.Scheme);
+            //// Create the reset password link :
+            //var resetLink = Url.Action("ResetPassword", "Account", new { email = user.Email, token }, Request.Scheme);
+
+            //// Send email
+            //await _emailService.SendEmailAsync(user.Email, "Reset Password",
+            //    $"Click here to reset your password: <a href='https://localhost:7027/Reset-password?userId={user.Email}&token={token}'>Reset Password</a>");
+            //return Ok(new { message = "Password reset link has been sent to your email" });
+            //---------------------------------
+            // Create the reset password code of 6 nums : 
+            var verificationCode = new Random().Next(100000, 999999).ToString();
+
+
+            _cache.Set(model.Email, verificationCode, TimeSpan.FromMinutes(5));
 
             // Send email
-            await _emailService.SendEmailAsync(user.Email, "Reset Password",
-                $"Click here to reset your password: <a href='https://localhost:7027/Reset-password?userId={user.Email}&token={token}'>Reset Password</a>");
-            return Ok(new { message = "Password reset link has been sent to your email" });
+            await _emailService.SendEmailAsync(user.Email, "Password Reset Code",
+                $"Your reset code is: {verificationCode}");
+
+            return Ok(new { message = "Password reset code has been sent to your email" });
         }
         #endregion
 
         #region Reset passward
+        /*
         [HttpPost("Resetpassword")]
         public async Task<IActionResult> ResetPassword([FromBody] ResetPasswordDto model)
         {
@@ -196,7 +190,70 @@ namespace Blink_API.Controllers.Account
 
             return BadRequest(new { message = "Enable to reset password" });
         }
+        */
 
+        #endregion
+
+        // after forget pass and enter el email , verify with code sent : 
+        #region verify code
+        [HttpPost("verifyCode")]
+        public async Task<IActionResult> VerifyCode([FromBody] VerifyCodeDto model)
+        {
+            if (!_cache.TryGetValue(model.Email, out string savedCode))
+            {
+                return BadRequest(new { message = "Code expired or not found", isValid = false });
+            }
+
+            if (model.code == savedCode)
+            {
+                return Ok(new { message = "Valid code", isValid = true });
+            }
+            else
+            {
+                return BadRequest(new { message = "Invalid code", isValid = false });
+            }
+        }
+        #endregion
+
+        // set new password :
+        #region set new password
+
+        [HttpPost("setNewPassword")]
+        public async Task<IActionResult> SetNewPassword([FromBody] SetNewPasswordDto model)
+        {
+
+            var user = await _userManager.GetUserAsync(User);   
+
+            if (user == null)
+            {
+                return BadRequest(new { message = "User not found" });
+            }
+
+            // check matching passwords :
+            if (model.NewPassword != model.ConfirmPassword)
+            {
+                return BadRequest(new { message = "Passwords do not match" });
+            }
+
+      // remove old pass and update it then:
+            var result = await _userManager.RemovePasswordAsync(user);  
+            if (result.Succeeded)
+            {
+                var updateResult = await _userManager.AddPasswordAsync(user, model.NewPassword);  
+                if (updateResult.Succeeded)
+                {
+                    return Ok(new { message = "Password has been successfully updated" });
+                }
+                else
+                {
+                    return BadRequest(new { message = "Unable to update password" });
+                }
+            }
+            else
+            {
+                return BadRequest(new { message = "Unable to remove old password" });
+            }
+        }
         #endregion
     }
 }
