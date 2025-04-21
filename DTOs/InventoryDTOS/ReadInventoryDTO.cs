68e2de091f4129ada666c4ebcea75d510ca9c63d
proGamalSherif <jemyxsherifx@gmail.com>
2025-04-21 08:17:45 +0200

    first updates on inventory

--- DTOs/InventoryDTOS/ReadInventoryDTO.cs
+++ DTOs/InventoryDTOS/ReadInventoryDTO.cs
@@ -6,6 +6,7 @@
         public string InventoryName { get; set; }
         public string InventoryAddress { get; set; }
         public string Phone { get; set; }
+        public int BranchId { get; set; }
         public string BranchName { get; set; }
         public decimal Lat { get; set; }
         public decimal Long { get; set; }
